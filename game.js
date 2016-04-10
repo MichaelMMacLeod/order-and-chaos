0d1214cde9a2e5052280bab94482cf8a7ae99a03
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-04-09 19:02:53 -0700

    Add canvas

--- game.js
+++ game.js
@@ -1,3 +1,28 @@
 startGame = function() {
-	console.log("Working!");
+	gameArea.start();
+}
+
+gameArea = {
+	canvas : document.createElement('canvas'),
+	start : function() {
+		this.canvas.width = 500;
+		this.canvas.height = 500;
+		this.context = this.canvas.getContext("2d");
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea, 20);
+		window.addEventListener('keydown', function (e) {
+			gameArea.keys = (gameArea.keys || []);
+			gameArea.keys[e.keyCode] = true;
+		})
+		window.addEventListener('keyup', function (e){
+			gameArea.keys[e.keyCode] = false;
+		})
+	},
+	clear : function() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+
+updateGameArea = function() {
+	gameArea.clear();
 }
