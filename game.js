7b0fd3aa0603bc741eefbdde85f1949dac01e440
Michael MacLeod <MichaelMMacLeod@gmail.com>
2016-04-22 22:39:50 -0700

    Make AI order complete diagonal right down sets of pieces

--- game.js
+++ game.js
@@ -96,6 +96,33 @@ function aiOrderTurn() {
 				}
 			}
 			counter = 0;
+			for (var k = 0; k < 5; k++) {
+				try {
+					if (matrix[i + k][j + k].color == "red") { // Diagonal right down, red
+						counter++;
+					}
+					if (matrix[i + k][j + k].color == "blue") { // Diagonal right down, blue
+						counter--;
+					}
+				} catch (err) { counter = 0; }
+			}
+			if (counter == 4) { // Diagonal right down, red
+				for (var k = 0; k < 5; k++) {
+					if (matrix[i + k][j + k] == 0) {
+						red = new aiPiece("red.png", "red", i + k, j + k);
+						aiPlaceable = false;
+					}
+				}
+			}
+			if (counter == -4) { // Diagonal right down, blue
+				for (var k = 0; k < 5; k++) {
+					if (matrix[i + k][j + k] == 0) {
+						blue = new aiPiece("blue.png", "blue", i + k, j + k);
+						aiPlaceable = false;
+					}
+				}
+			}
+			counter = 0;
 		}
 	}
 }
